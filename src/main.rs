6570b44c9d826c1533e36dbd6f1ab16ab2c9da31
craftzman <chaixxv@gmail.com>
2023-08-06 23:36:36 +0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use tokio::net::TcpListener;
+
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+
+    let (socket , _addr) = listener.accept().await.unwrap();
+}
