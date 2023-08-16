ab27b9c20a5c489d2e21f143fd21bb318d4fef45
craftzman <chaixxv@gmail.com>
2023-08-16 22:12:23 +0700

    Add buffer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
-use tokio::net::TcpListener;
+use tokio::{net::TcpListener, io::{AsyncReadExt, AsyncWriteExt}};
 
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let (socket , _addr) = listener.accept().await.unwrap();
+    let (mut socket , _addr) = listener.accept().await.unwrap();
+
+    let mut buffer = [0u8;1024];
+
+    let bytes_read = socket.read(&mut buffer).await.unwrap();
+
+    socket.write(&buffer[..bytes_read]).await.unwrap();
 }
